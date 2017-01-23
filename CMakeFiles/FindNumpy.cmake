8cab1a06cba8cc4ff3ab1a2bcd2e771e70b8d1e9
Hamdi Sahloul <hamdisahloul@hotmail.com>
2017-01-23 12:05:06 +0900

    Organize and cleanup CMake dependencies

--- CMakeFiles/FindNumpy.cmake
+++ CMakeFiles/FindNumpy.cmake
@@ -10,7 +10,7 @@
 
 
 exec_program ("${PYTHON_EXECUTABLE}"
-ARGS "-c 'import numpy; print numpy.get_include()'"
+ARGS "-c \"import numpy; print numpy.get_include()\""
 OUTPUT_VARIABLE NUMPY_INCLUDE_DIR
 RETURN_VALUE NUMPY_NOT_FOUND)
 
